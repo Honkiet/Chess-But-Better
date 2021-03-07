333be1f618a67aeb48cd427ca6f66c370ee5de51
Hendrik Truong <truheco@gmail.com>
2021-03-07 19:32:07 +0100

    Created Nav mesh and started with a baseAi that can runaway or move. (To Do: radius for the ai)

---
+++ ChessAI/Assets/Scripts/BaseAi.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseAi : MonoBehaviour
+{
+    
+    NavMeshAgent agent;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunAway(target.transform.position);
+    }
+
+    void Chase(Vector3 destination)
+    {
+        agent.SetDestination(destination);
+    }
+
+    void RunAway(Vector3 destination)
+    {
+        Vector3 AwayVector = destination - this.transform.position;
+        agent.SetDestination(this.transform.position - AwayVector);
+    }
+}
