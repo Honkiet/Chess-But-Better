c0346c5742b91b02f5dfd198e57341cf8eaac661
Hendrik Truong <truheco@gmail.com>
2021-03-10 16:52:11 +0100

    Added Brackeys Healthbar system

--- ChessAI/Assets/Scripts/Piece.cs
+++ ChessAI/Assets/Scripts/Piece.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    [SerializeField] float healthPoints = 100f;
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] int currentHealth;
     [SerializeField] float visionDistance = 20.0f;
     [SerializeField] float visionAngle = 30.0f;
     [SerializeField] float dmg = 10f;
+    [SerializeField] HealthBar healthBar;
+
 
     bool isDead = false;
 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
     public bool IsDead()
     {
         return isDead;
     }
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
-        healthPoints = Mathf.Max(healthPoints - damage, 0);
-        if (healthPoints == 0)
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth == 0)
         {
             Die();
         }
